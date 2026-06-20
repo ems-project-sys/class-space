4a71c9c11d6c997e6b0c868cabeb04193bf4b6f3
Kris <kzadka89@gmail.com>
2026-06-21 00:07:50 +0200

    feat: implement rooms controller

--- classpace-backend/src/rooms/rooms.controller.ts
+++ classpace-backend/src/rooms/rooms.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RoomsService, Room } from './rooms.service';
 
-@Controller('rooms')
-export class RoomsController {}
+@Controller('api/rooms')
+export class RoomsController {
+    constructor(private readonly roomsService: RoomsService) {}
+
+    @Get()
+    getAllRooms(): Room[] {
+        return this.roomsService.findAll();
+    }
+}
