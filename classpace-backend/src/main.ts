d5bf70af5298899bf2726974c0e5f369241ca114
Kris <kzadka89@gmail.com>
2026-06-21 00:09:44 +0200

    feat: implement CORS config

--- classpace-backend/src/main.ts
+++ classpace-backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+// Enable CORS so the React frontend can fetch data
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
